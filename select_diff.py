66c7b109dbd0672bb53f011f96a1dc201232aaf1
Shaba Abhiram <shabarivas@gmail.com>
2014-12-17 21:33:47 -0800

    slightly more interesting plugin code :)

--- select_diff.py
+++ select_diff.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 
-def some_method():
+import sublime
+import sublime_plugin
+
+class SampleCommand(sublime_plugin.TextCommand):
     """
-    Initial placeholder
+    Sample Command to play w/ sublime text stuffs
     """
-    return True
+
+    def run(self, edit):
+        new_view = sublime.active_window().new_file()
+        new_view.insert(edit, 0, "Hello")
+
+
+def helper_fn(x):
+    """
+    Dummy helper function, to help validate that the
+    tests can also hit non-sublime_plugin classes
+    """
+    return x
